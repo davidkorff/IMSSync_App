18530ad839028ca815ea360640c0d070e65b70da
davidkorff <davidkorff@gmail.com>
2025-07-28 23:55:44 -0400

    1

--- app/services/ims/__init__.py
+++ app/services/ims/__init__.py
@@ -1,9 +1,9 @@
 from .auth_service import IMSAuthService as AuthService
-from .insured_service import InsuredService
-from .quote_service import QuoteService
+from .insured_service import IMSInsuredService as InsuredService
+from .quote_service import IMSQuoteService as QuoteService
 from .invoice_service import InvoiceService
 from .base_service import BaseIMSService
-from .data_access_service import DataAccessService
+from .data_access_service import IMSDataAccessService as DataAccessService
 
 __all__ = [
     'AuthService',
